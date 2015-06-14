564176bfc815fa738cf4e7421491a4f83a1d66ea
win <wintercoredev@gmail.com>
2015-06-14 23:53:37 +0700

    16: HtmlTokenFactory

---
+++ HtmlKit/HtmlTokenFactory.cs
@@ -0,0 +1,39 @@
+﻿//2015, MIT, WinterDev
+
+using System;
+
+namespace HtmlKit
+{
+    public abstract class HtmlTokenFactory
+    {
+        public abstract HtmlToken CreateCommentToken(string comment);
+        public abstract HtmlToken CreateHtmlDataToken(string data);
+        public abstract HtmlToken CreateHtmlTagToken(string name, bool isEndTag);
+        public abstract HtmlToken CreateHtmlDocTypeToken();
+
+    }
+
+    public class MyHtmlTokenFactory : HtmlTokenFactory
+    {
+        public override HtmlToken CreateCommentToken(string comment)
+        {
+            return new HtmlCommentToken(comment);
+        }
+
+        public override HtmlToken CreateHtmlDataToken(string data)
+        {
+            return new HtmlDataToken(data);
+        }
+
+        public override HtmlToken CreateHtmlDocTypeToken()
+        {
+            return new HtmlDocTypeToken();
+        }
+
+        public override HtmlToken CreateHtmlTagToken(string name, bool isEndTag)
+        {
+            return new HtmlTagToken(name, isEndTag);
+        }
+    }
+
+}
