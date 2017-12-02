becaa3503f82b7212ee337e5267901c8f32c9743
Jeffrey Stedfast <jestedfa@microsoft.com>
2017-12-02 12:35:28 -0500

    bumped version to 1.0.1

--- HtmlKit/Properties/AssemblyInfo.cs
+++ HtmlKit/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@
 //
 // Author: Jeffrey Stedfast <jeff@xamarin.com>
 //
-// Copyright (c) 2015-2016 Xamarin Inc. (www.xamarin.com)
+// Copyright (c) 2015-2017 Xamarin Inc. (www.xamarin.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -35,7 +35,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("Xamarin Inc.")]
 [assembly: AssemblyProduct ("HtmlKit")]
-[assembly: AssemblyCopyright ("Copyright © 2015-2016 Xamarin Inc. (www.xamarin.com)")]
+[assembly: AssemblyCopyright ("Copyright © 2015-2017 Xamarin Inc. (www.xamarin.com)")]
 [assembly: AssemblyTrademark ("Xamarin Inc.")]
 [assembly: AssemblyCulture ("")]
 
@@ -77,6 +77,6 @@ using System.Runtime.InteropServices;
 //
 // If there have only been bug fixes, bump the Micro Version and/or the Build Number
 //    in the AssemblyFileVersion attribute.
-[assembly: AssemblyInformationalVersion ("1.0.0.0")]
-[assembly: AssemblyFileVersion ("1.0.0.0")]
+[assembly: AssemblyInformationalVersion ("1.0.1.0")]
+[assembly: AssemblyFileVersion ("1.0.1.0")]
 [assembly: AssemblyVersion ("1.0.0.0")]
