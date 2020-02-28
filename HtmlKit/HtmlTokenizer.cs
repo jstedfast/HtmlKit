9428ba676cd0297416641a6fd8d23bebe4d1090f
Jeffrey Stedfast <jestedfa@microsoft.com>
2020-02-28 18:08:56 -0500

    Fixed doc comment

--- HtmlKit/HtmlTokenizer.cs
+++ HtmlKit/HtmlTokenizer.cs
@@ -74,8 +74,8 @@ namespace HtmlKit {
 		/// </summary>
 		/// <remarks>
 		/// <para>Gets or sets whether or not the tokenizer should decode character references.</para>
-		/// <para><alert class="warning">Character references in attribute values will still be decoded
-		/// even if this value is set to <c>false</c>.</alert></para>
+		/// <note class="warning">Character references in attribute values will still be decoded
+		/// even if this value is set to <c>false</c>.</node>
 		/// </remarks>
 		/// <value><c>true</c> if character references should be decoded; otherwise, <c>false</c>.</value>
 		public bool DecodeCharacterReferences {
