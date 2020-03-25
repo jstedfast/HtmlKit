10d010c1b325ea8cd6ce837286d47759a993cf44
Jeffrey Stedfast <jestedfa@microsoft.com>
2020-03-25 08:09:19 -0400

    Optimized the HtmlEntityDecoder state machine

--- CodeGenerator/Program.cs
+++ CodeGenerator/Program.cs
@@ -146,12 +146,21 @@ namespace CodeGenerator {
 				}
 			}
 
+			GenerateSwitchLabels (Root);
+
 			using (var output = new StreamWriter ("HtmlEntityDecoder.g.cs")) {
 				output.WriteLine ("// WARNING: This file is auto-generated. DO NOT EDIT!");
 				output.WriteLine ();
+				output.WriteLine ("using System.Collections.Generic;");
+				output.WriteLine ();
 				output.WriteLine ("namespace HtmlKit {");
-				output.WriteLine ("\tpublic partial class HtmlEntityDecoder {");
+				output.WriteLine ("\tpublic partial class HtmlEntityDecoder");
+				output.WriteLine ("\t{");
 				output.WriteLine ("\t\tconst int MaxEntityLength = {0};", maxEntityLength);
+				output.WriteLine ();
+				GenerateStaticConstructor (output);
+				output.WriteLine ();
+				GenerateBinarySearchMethod (output);
 				output.WriteLine ();
 				GeneratePushNamedEntityMethod (output);
 				output.WriteLine ();
@@ -177,28 +186,107 @@ namespace CodeGenerator {
 			}
 		}
 
+		static string GetTransitionTableName (char c)
+		{
+			return string.Format ("TransitionTable_{0}", c == ';' ? "semicolon" : c.ToString ());
+		}
+
+		static void GenerateStaticConstructor (TextWriter output)
+		{
+			int index;
+
+			output.WriteLine ("\t\tstatic readonly Dictionary<int, string> NamedEntities;");
+			output.WriteLine ();
+			output.WriteLine ("\t\tstruct Transition");
+			output.WriteLine ("\t\t{");
+			output.WriteLine ("\t\t\tpublic readonly int From;");
+			output.WriteLine ("\t\t\tpublic readonly int To;");
+			output.WriteLine ();
+			output.WriteLine ("\t\t\tpublic Transition (int from, int to)");
+			output.WriteLine ("\t\t\t{");
+			output.WriteLine ("\t\t\t\tFrom = from;");
+			output.WriteLine ("\t\t\t\tTo = to;");
+			output.WriteLine ("\t\t\t}");
+			output.WriteLine ("\t\t}");
+			output.WriteLine ();
+			foreach (var outer in OuterSwitchLabels)
+				output.WriteLine ("\t\tstatic readonly Transition[] {0};", GetTransitionTableName (outer.Key));
+			output.WriteLine ();
+			output.WriteLine ("\t\tstatic HtmlEntityDecoder ()");
+			output.WriteLine ("\t\t{");
+			foreach (var outer in OuterSwitchLabels) {
+				var tableName = GetTransitionTableName (outer.Key);
+				index = 0;
+
+				output.WriteLine ("\t\t\t{0} = new Transition[{1}] {{", tableName, outer.Value.Count);
+				foreach (var state in outer.Value) {
+					var comma = index + 1 < outer.Value.Count ? "," : string.Empty;
+					var value = state.Value;
+
+					output.WriteLine ("\t\t\t\tnew Transition ({0}, {1}){2} // {3}", value.CurrentState, value.NextState, comma, value.Comment);
+					index++;
+				}
+				output.WriteLine ("\t\t\t};");
+			}
+			output.WriteLine ();
+			output.WriteLine ("\t\t\tNamedEntities = new Dictionary<int, string> {");
+			index = 0;
+			foreach (var kvp in FinalStates) {
+				var comma = index < FinalStates.Count ? "," : string.Empty;
+				var state = kvp.Value;
+
+				output.Write ("\t\t\t\t[{0}] = \"", state.State);
+				for (int i = 0; i < state.Value.Length; i++)
+					output.Write ("\\u{0:X4}", (int) state.Value[i]);
+				output.WriteLine ("\"{0} // {1}", comma, state.Name);
+				index++;
+			}
+			output.WriteLine ("\t\t\t};");
+			output.WriteLine ("\t\t}");
+		}
+
+		static void GenerateBinarySearchMethod (TextWriter output)
+		{
+			output.WriteLine ("\t\tstatic int BinarySearchNextState (Transition[] transitions, int state)");
+			output.WriteLine ("\t\t{");
+			output.WriteLine ("\t\t\tint min = 0, max = transitions.Length;");
+			output.WriteLine ();
+			output.WriteLine ("\t\t\tif (max == 0)");
+			output.WriteLine ("\t\t\t\treturn -1;");
+			output.WriteLine ();
+			output.WriteLine ("\t\t\tdo {");
+			output.WriteLine ("\t\t\t\tint i = min + ((max - min) / 2);");
+			output.WriteLine ();
+			output.WriteLine ("\t\t\t\tif (state > transitions[i].From) {");
+			output.WriteLine ("\t\t\t\t\tmin = i + 1;");
+			output.WriteLine ("\t\t\t\t} else if (state < transitions[i].From) {");
+			output.WriteLine ("\t\t\t\t\tmax = i;");
+			output.WriteLine ("\t\t\t\t} else {");
+			output.WriteLine ("\t\t\t\t\treturn transitions[i].To;");
+			output.WriteLine ("\t\t\t\t}");
+			output.WriteLine ("\t\t\t} while (min < max);");
+			output.WriteLine ();
+			output.WriteLine ("\t\t\treturn -1;");
+			output.WriteLine ("\t\t}");
+		}
+
 		static void GeneratePushNamedEntityMethod (TextWriter output)
 		{
-			GenerateSwitchLabels (Root);
-
 			output.WriteLine ("\t\tbool PushNamedEntity (char c)");
 			output.WriteLine ("\t\t{");
-			output.WriteLine ("\t\t\tint state = states[index - 1];");
+			output.WriteLine ("\t\t\tint next, state = states[index - 1];");
+			output.WriteLine ("\t\t\tTransition[] table = null;");
 			output.WriteLine ();
 			output.WriteLine ("\t\t\tswitch (c) {");
-			foreach (var outer in OuterSwitchLabels) {
-				output.WriteLine ("\t\t\tcase '{0}':", outer.Key);
-				output.WriteLine ("\t\t\t\tswitch (state) {");
-				foreach (var state in outer.Value)
-					output.WriteLine ("\t\t\t\tcase {0}: state = {1}; break; // {2}", state.Value.CurrentState, state.Value.NextState, state.Value.Comment);
-				output.WriteLine ("\t\t\t\tdefault: return false;");
-				output.WriteLine ("\t\t\t\t}");
-				output.WriteLine ("\t\t\t\tbreak;");
-			}
+			foreach (var outer in OuterSwitchLabels)
+				output.WriteLine ("\t\t\tcase '{0}': table = {1}; break;", outer.Key, GetTransitionTableName (outer.Key));
 			output.WriteLine ("\t\t\tdefault: return false;");
-			output.WriteLine ("\t\t\t}"); // end switch (state)
+			output.WriteLine ("\t\t\t}"); // end switch (c)
 			output.WriteLine ();
-			output.WriteLine ("\t\t\tstates[index] = state;");
+			output.WriteLine ("\t\t\tif ((next = BinarySearchNextState (table, state)) == -1)");
+			output.WriteLine ("\t\t\t\treturn false;");
+			output.WriteLine ();
+			output.WriteLine ("\t\t\tstates[index] = next;");
 			output.WriteLine ("\t\t\tpushed[index] = c;");
 			output.WriteLine ("\t\t\tindex++;");
 			output.WriteLine ();
@@ -208,28 +296,13 @@ namespace CodeGenerator {
 
 		static void GenerateGetNamedEntityValueMethod (TextWriter output)
 		{
-			output.WriteLine ("\t\tstatic string GetNamedEntityValue (int state)");
-			output.WriteLine ("\t\t{");
-			output.WriteLine ("\t\t\tswitch (state) {");
-			foreach (var kvp in FinalStates) {
-				var state = kvp.Value;
-
-				output.Write ("\t\t\tcase {0}: return \"", state.State);
-				for (int i = 0; i < state.Value.Length; i++)
-					output.Write ("\\u{0:X4}", (int) state.Value[i]);
-				output.WriteLine ("\"; // {0}", state.Name);
-			}
-			output.WriteLine ("\t\t\tdefault: return null;");
-			output.WriteLine ("\t\t\t}");
-			output.WriteLine ("\t\t}");
-			output.WriteLine ();
 			output.WriteLine ("\t\tstring GetNamedEntityValue ()");
 			output.WriteLine ("\t\t{");
 			output.WriteLine ("\t\t\tint startIndex = index;");
 			output.WriteLine ("\t\t\tstring decoded = null;");
 			output.WriteLine ();
 			output.WriteLine ("\t\t\twhile (startIndex > 0) {");
-			output.WriteLine ("\t\t\t\tif ((decoded = GetNamedEntityValue (states[startIndex - 1])) != null)");
+			output.WriteLine ("\t\t\t\tif (NamedEntities.TryGetValue (states[startIndex - 1], out decoded))");
 			output.WriteLine ("\t\t\t\t\tbreak;");
 			output.WriteLine ();
 			output.WriteLine ("\t\t\t\tstartIndex--;");
